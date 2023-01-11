3e6b285405344b0df4aec2aa0cfbb4cf320c7f26
Alvin Galit <aroblesgalit@gmail.com>
2023-01-10 22:50:59 -0800

    Learned different types

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,63 @@
+// Basic Types
 let id: number = 5
+let company: string = 'Traversy Media'
+let isPublished: boolean = true
+let x: any = 'Hello'
+let age: number
+
+let ids: number[] = [1, 2, 3]
+let arr: any[] = [1, true, 'Hello']
+
+// Tuple
+let person: [number, string, boolean] = [1, 'Brad', true]
+// Tuple Array
+let employee: [number, string][]
+
+employee = [
+    [1, 'Brad'],
+    [2, 'John'],
+    [3, 'Jill']
+]
+
+// Union
+let pid: string | number
+pid = 22
+
+// Enum
+enum Direction1 {
+    Up = 1, 
+    Down,
+    Left,
+    Right
+}
+
+enum Direction2 {
+    Up = 'Up', 
+    Down = 'Down',
+    Left = 'Left',
+    Right = 'Right'
+}
+
+// Objects
+type User = {
+    id: number,
+    name: string
+}
+const user: User = {
+    id: 1,
+    name: 'John'
+}
+
+// Type Assertion
+let cid: any = 1
+// let customerId = <number>cid
+let customerId = cid as number
+
+// Functions
+function addNum(x: number, y: number): number {
+    return x + y
+}
+
+function log(message: string | number): void {
+    console.log(message)
+}
