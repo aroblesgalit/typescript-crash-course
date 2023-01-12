c2cd553d8344a67237d1b444ea73b7009b35decd
Alvin Galit <aroblesgalit@gmail.com>
2023-01-11 20:56:09 -0800

    Learned about classes

--- src/index.ts
+++ src/index.ts
@@ -82,3 +82,22 @@ interface MathFunc {
 const add: MathFunc = (x: number, y: number): number => x + y
 const sub: MathFunc = (x: number, y: number): number => x - y
 
+// Classes
+class Person {
+    id: number // 'private' and 'protected' makes it so it can only be accessible inside the class => private id: number
+    name: string
+
+    constructor(id: number, name: string) {
+        this.id = id
+        this.name = name
+    }
+
+    register() {
+        return `${this.name} is now registered`
+    }
+}
+
+const brad = new Person(1, 'Brad Traversy')
+const mike = new Person(2, 'Mike Jordan')
+
+console.log(brad.register())
