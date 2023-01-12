17b7e7bea1845a99ba22530f0ed2fca86a847bd2
Alvin Galit <aroblesgalit@gmail.com>
2023-01-11 21:06:00 -0800

    Learned about generics

--- dist/index.js
+++ dist/index.js
@@ -76,4 +76,9 @@ class Employee extends Person {
     }
 }
 const emp = new Employee(3, 'Shawn', 'Developer');
-console.log(emp.register());
+// Generics
+function getArray(items) {
+    return new Array().concat(items);
+}
+let numArray = getArray([1, 2, 3, 4]);
+let strArray = getArray(['brad', 'john', 'jill']);
--- src/index.ts
+++ src/index.ts
@@ -118,4 +118,10 @@ class Employee extends Person {
 
 const emp = new Employee(3, 'Shawn', 'Developer')
 
-console.log(emp.register())
+// Generics
+function getArray<T>(items: T[]): T[] {
+    return new Array().concat(items)
+}
+
+let numArray = getArray<number>([1, 2, 3, 4])
+let strArray = getArray<string>(['brad', 'john', 'jill'])
